e829e324466f1a87b66c9b2ecf056233cc3700ce
kanapuli <athavankanapuli@gmail.com>
2019-07-23 10:27:47 +0530

    Stop allocating extra junk in the tail of the list.

--- src/bad_list.rs
+++ src/bad_list.rs
@@ -1,10 +1,18 @@
+struct Node {
+    elem: i32,
+    next: Link,
+}
 //pub allows this enum to be publicly available
-pub enum List {
+enum Link {
     Empty,
     //First approach was Elem(i32, List)
     //But since List size depends on the elemnts it contains
     //So when List is recursively mentioned, rust complains
     //Hence use a Box, which allocates a defined memory size
     // in heap and returns the ownership.
-    Elem(i32, Box<List>),
+    More(Box<Node>),
+}
+
+pub struct List {
+    head: Link,
 }
