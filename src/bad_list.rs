eaaa1c8a281d49309344622dcf7577871af5916a
kanapuli <athavankanapuli@gmail.com>
2019-07-24 23:08:37 +0530

    Implement push function. Use mem::replace to fix 'move out of borrowed content'

--- src/bad_list.rs
+++ src/bad_list.rs
@@ -1,3 +1,4 @@
+use std::mem;
 struct Node {
     elem: i32,
     next: Link,
@@ -19,7 +20,20 @@ pub struct List {
 
 //Implementation for the struct List
 impl List {
+    //new is a static method
     pub fn new() -> Self {
+        //refer tags/variants of enum using ::
+        //Last expression of function in implicitly returned
         List { head: Link::Empty }
+    }
+
+    //push is a non static method.
+    //Since it has self as an argument.
+    pub fn push(&mut self, elem: i32) {
+        let node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+        self.head = Link::More(node);
     }
 }
