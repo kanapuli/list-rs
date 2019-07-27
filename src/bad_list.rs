76c0ac495349846cfb51489303cca961a233b12b
kanapuli <athavankanapuli@gmail.com>
2019-07-27 23:18:44 +0530

    Implement Drop destructor for the List

--- src/bad_list.rs
+++ src/bad_list.rs
@@ -49,6 +49,16 @@ impl List {
     }
 }
 
+impl Drop for List {
+    fn drop(&mut self) {
+        //    self.head.drop();
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+    }
+}
+
 mod test {
     use super::List;
     #[test]
