7e6978d21f3c249dedf898679268ac1b5c1eadb8
kanapuli <athavankanapuli@gmail.com>
2019-07-28 23:20:21 +0530

    Remove mem::replace with Option's take method. This makes the code to read better

--- src/better_list.rs
+++ src/better_list.rs
@@ -20,12 +20,12 @@ impl List {
     pub fn push(&mut self, elem: i32) {
         let new_node = Box::new(Node {
             elem: elem,
-            next: mem::replace(&mut self.head, None),
+            next: self.head.take(),
         });
-        self.head = new_node;
+        self.head = Some(new_node);
     }
     pub fn pop() -> Option<i32> {
-        match mem::replace(&mut self.head, None) {
+        match self.head.take() {
             None => None,
             Some(node) => {
                 self.head = node.next,
@@ -37,9 +37,9 @@ impl List {
 
 impl Drop for List {
     fn drop(&mut self) {
-        let mut cur_link = mem::replace(&mut self.head, None) ;
+        let mut cur_link = self.head.take();
         while let Some(mut boxed_node) = cur_link {
-            cur_link = mem::replace(&mut boxed_node.next, None) ;
+            cur_link = boxed_node.next.take();
         }
     }
 }
