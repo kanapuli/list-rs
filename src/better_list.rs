b0dba3af2c543cbf7077d21627820de6972ca59a
kanapuli <athavankanapuli@gmail.com>
2019-07-29 23:25:09 +0530

    Support generic types for the linked list

--- src/better_list.rs
+++ src/better_list.rs
@@ -7,24 +7,24 @@ pub struct List<T> {
 //type Aliases
 type Link<T> = Option<Box<Node<T>>>;
 
-struct Node {
+struct Node<T> {
     elem: T,
     next: Link<T>,
 }
 
-impl List {
-    pub fn new() -> List {
+impl<T> List<T> {
+    pub fn new() -> Self {
         List { head: None }
     }
 
-    pub fn push(&mut self, elem: i32) {
+    pub fn push(&mut self, elem: T) {
         let new_node = Box::new(Node {
             elem: elem,
             next: self.head.take(),
         });
         self.head = Some(new_node);
     }
-    pub fn pop(&mut self) -> Option<i32> {
+    pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|node| {
             self.head = node.next;
             node.elem
@@ -32,7 +32,7 @@ impl List {
     }
 }
 
-impl Drop for List {
+impl<T> Drop for List<T> {
     fn drop(&mut self) {
         let mut cur_link = self.head.take();
         while let Some(mut boxed_node) = cur_link {
